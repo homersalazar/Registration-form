d0829bf9d85447246449f8fc34bbd9b5533aa52e
homer <salazarhomer1@gmail.com>
2023-08-17 10:58:43 +0800

    adding loading in index

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
     require_once("./includes/header.php");
 ?>
-    <div class="flex justify-center items-center h-screen">
-        <h1 class="text-5xl">HELLO</h1>
+    <div class="flex flex-col justify-center items-center h-screen gap-8">
+        <h1 class="text-5xl">LOGO</h1>
+        <p>Title</p>
+        <div class="three-body">
+            <div class="three-body__dot"></div>
+            <div class="three-body__dot"></div>
+            <div class="three-body__dot"></div>
+        </div>
     </div>
     <script type="module" src="./assets/js/index.js"></script>
 <?php 
